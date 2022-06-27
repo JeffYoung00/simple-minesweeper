c8c195d063f01a9717d7d2b65b9caeedde8aada6
jiangyiqing <1181385834@qq.com>
2022-06-27 09:58:11 +0800

    Bug fix

--- Simple Minesweeper/Game/Jyq/Sever.java
+++ Simple Minesweeper/Game/Jyq/Sever.java
@@ -3,6 +3,7 @@ package Jyq;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.SocketException;
 
 public class Sever {
     private ServerSocket serverSocket;
@@ -25,19 +26,35 @@ public class Sever {
                 System.out.println("Name:" + name);
                 System.out.println("LoginTime:" + LoginTime);
                 System.out.println("IP:" + IP);
-                Time = bufferedReader.readLine();
-                System.out.println("GamingTime:" + Time);
-                RankingList rankingList =  (LazyUtils.ReadObject(Directories.RankingList, RankingList.class));
-                rankingList.addRank(new Rank(IP, Integer.parseInt(Time), name,LoginTime));
-                LazyUtils.WriteObject(Directories.RankingList, rankingList);
+                try {
+                    Time = bufferedReader.readLine();
+                    System.out.println("GamingTime:" + Time);
+                    RankingList rankingList = (LazyUtils.ReadObject(Directories.RankingList, RankingList.class));
+                    rankingList.addRank(new Rank(IP, Integer.parseInt(Time), name, LoginTime));
+                    LazyUtils.WriteObject(Directories.RankingList, rankingList);
+                }catch (SocketException e) {
+                    System.out.println("连接已断开，用户没有成功完成游戏！");
+                    this.Close();
+                }
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
-
+    public void Close() {
+        try {
+            serverSocket.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
     public static void main(String[] args) throws IOException {
         ServerSocket serverSocket = new ServerSocket(10024);
-        Sever sever = new Sever(serverSocket);
-        sever.StartSever();
+        try {
+            Sever sever = new Sever(serverSocket);
+            sever.StartSever();
+            sever.Close();
+        }catch (Exception e) {
+            serverSocket.close();
+        }
     }
 }
